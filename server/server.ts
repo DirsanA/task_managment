06158bb2d178792a8840ca32759fc3497505e729
DirsanA <dirsannsr079814@students.wku.edu.et>
2025-08-27 14:12:53 -0700

    getting user from api end point

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/config.js";
-import userModel from "./model/userModel.js";
+import User from "./model/userModel.js"; // capitalized by convention
+
 dotenv.config();
 
 const app = express();
+app.use(express.json()); // parse JSON bodies
 
 const port: number = Number(process.env.PORT) || 3000;
+
 const tryToConnect = async (): Promise<void> => {
   try {
     await connectDB();
@@ -18,20 +21,47 @@ const tryToConnect = async (): Promise<void> => {
   }
 };
 
-app.post("/", function (req: any, res: any) {
-  const { name, email, password } = req.body;
-  if (!name || !email || password) {
-    return res.staus(400).send({
-      message: "All fild is required",
+// POST /post
+app.post("/post", async (req: any, res: any) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).send({
+        message: "All filds are required",
+      });
+    }
+    const newUser = new User({
+      name,
+      email,
+      password,
     });
+    await newUser.save();
+    return res.status(201).send({
+      message: "User is created",
+    });
+  } catch (error) {
+    console.log(error);
   }
-  const newUser = new userModel({
-    name,
-    email,
-    password,
-  });
-  if (newUser) {
-    return res.staus(201).send("user is created");
+});
+
+// get user
+
+app.get("/get", async (req: any, res: any) => {
+  try {
+    const user = await User.find({});
+    if (user.length === 0) {
+      console.log("user not found");
+      return res.status(404).send({
+        message: "User not found",
+        data: [],
+      });
+    }
+    return res.status(200).send({
+      message: "user found",
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
   }
 });
 
